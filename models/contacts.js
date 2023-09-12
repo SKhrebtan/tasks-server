206089b22ce6a084a738a72ceb942dcbedec0521
SKhrebtan <shrebtan@gmail.com>
2023-09-12 18:13:29 +0300

    first configs

--- models/contacts.js
+++ models/contacts.js
@@ -1,19 +1,58 @@
-// const fs = require('fs/promises')
+const fs = require("fs/promises");
+const path = require("path");
+const { nanoid } = require('nanoid')
 
-const listContacts = async () => {}
+const contactsPath = path.join(__dirname, "./contacts.json");
+console.log(contactsPath)
+const listContacts = async() => {
+    const list = await fs.readFile(contactsPath);
+    return JSON.parse(list);
+}
 
-const getContactById = async (contactId) => {}
+const getContactById = async (id) => {
+    const list = await listContacts();
+    const contact = list.find(t => t.id === id)
+    return contact || null;
+}
 
-const removeContact = async (contactId) => {}
+const addContact = async (data) => {
+    const list = await listContacts();
+    const newContact = {
+        id: nanoid(),
+        ...data
+    };
+    list.push(newContact);
+    await fs.writeFile(contactsPath, JSON.stringify(list, null, 2));
+    return newContact;
+}
 
-const addContact = async (body) => {}
+const removeContact = async (id) => {
+    const list = await listContacts();
+    const contactIndex = list.findIndex(t => t.id === id)
+    if (contactIndex === -1) {
+        return null;
+    }
+    const [result] = list.splice(contactIndex, 1);
+    await fs.writeFile(contactsPath, JSON.stringify(list, null, 2));
+    return result;
+}
 
-const updateContact = async (contactId, body) => {}
+const updatedContact = async (id, data) => {
+    const list = await listContacts();
+    const contactIndex = list.findIndex(t => t.id === id)
+    if (contactIndex === -1) {
+        return null;
+    }
+    list[contactIndex] = { id, ...data };
+    await fs.writeFile(contactsPath, JSON.stringify(list, null, 2));
+    return list[contactIndex];
+
+}
 
 module.exports = {
-  listContacts,
-  getContactById,
-  removeContact,
-  addContact,
-  updateContact,
+    listContacts,
+    getContactById,
+    addContact,
+    removeContact,
+    updatedContact
 }
